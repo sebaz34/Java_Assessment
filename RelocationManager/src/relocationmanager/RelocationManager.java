fa3c7b1922c430ba769a464372c14219adae82c4
sebaz34 <68626000+sebaz34@users.noreply.github.com>
2021-03-08 11:52:38 +1000

    SWAP IP Address with Relocation Manager
    Relocation Manager is actually the assingment not IP Address

--- RelocationManager/src/relocationmanager/RelocationManager.java
+++ RelocationManager/src/relocationmanager/RelocationManager.java
@@ -1,4 +1,4 @@
-package ipaddressdatabase;
+package relocationmanager;
 
 // With Sample Modularisation
 
@@ -11,25 +11,26 @@ import javax.swing.SpringLayout;
  * @author Mark O'Reilly
  */
 
-public class IPAddressDatabase extends Frame implements WindowListener
+public class RelocationManager extends Frame implements WindowListener
 {
-    Label lblPCName, lblPCID, lblIP, lblFind;
-    TextField txtPCName, txtPCID, txtIP, txtFind;
+    Label lblContactName, lblContactType, lblPhoneNumber, lblWebsiteEmail, lblContactNotes, lblFind;
+    TextField txtContactName, txtContactType, txtPhoneNumber, txtWebsiteEmail, txtFind;
+    TextArea txtContactNote;
     Button btnNew, btnSave, btnDel, btnFind, btnExit, btnFirst, btnPrev, btnNext, btnLast;
 
     public static void main(String[] args)
     {
-        Frame myFrame = new IPAddressDatabase();
-        myFrame.setSize(470,250);
+        Frame myFrame = new RelocationManager();
+        myFrame.setSize(600,350);
         myFrame.setLocation(400, 200);
         myFrame.setResizable(false);
         myFrame.setVisible(true);
     }
 
-    public IPAddressDatabase()
+    public RelocationManager()
     {
-        setTitle("IP Address Database");
-        setBackground(Color.yellow);
+        setTitle("Relocation Manager");
+        setBackground(Color.white);
 
         SpringLayout myLayout = new SpringLayout();
         setLayout(myLayout);
@@ -37,6 +38,7 @@ public class IPAddressDatabase extends Frame implements WindowListener
 	// Call the methods below to instantiate and place the various screen components
         LocateLabels(myLayout);
         LocateTextFields(myLayout);
+        LocateTextAreas(myLayout);
         LocateButtons(myLayout);
 
         this.addWindowListener(this);
@@ -47,10 +49,12 @@ public class IPAddressDatabase extends Frame implements WindowListener
     // Each line requests the LocateALabel method to instantiate, add and place a specific Label  
     public void LocateLabels(SpringLayout myLabelLayout)
     {
-        lblPCName = LocateALabel(myLabelLayout, lblPCName, "PC Name", 30, 25);
-        lblPCID = LocateALabel(myLabelLayout, lblPCID, "PC ID", 30, 50);
-        lblIP = LocateALabel(myLabelLayout, lblIP, "IP", 30, 75);
-        lblFind = LocateALabel(myLabelLayout, lblFind, "Search", 30, 120);
+        lblContactName = LocateALabel(myLabelLayout, lblContactName, "Contact Name:", 23, 25);
+        lblContactType = LocateALabel(myLabelLayout, lblContactType, "Contact Type:", 23, 50);
+        lblPhoneNumber = LocateALabel(myLabelLayout, lblPhoneNumber, "Phone Number:", 23, 75);
+        lblWebsiteEmail = LocateALabel(myLabelLayout, lblWebsiteEmail, "Website or Email:", 23, 100);
+        lblContactNotes = LocateALabel(myLabelLayout, lblContactNotes, "Contact Notes:", 23, 125);
+        lblFind = LocateALabel(myLabelLayout, lblFind, "Find", 390, 25);
     }
 
     /**
@@ -77,15 +81,16 @@ public class IPAddressDatabase extends Frame implements WindowListener
     // Each line requests the LocateATextField method to instantiate, add and place a specific TextField  
     public void LocateTextFields(SpringLayout myTextFieldLayout)
     {
-        txtPCName  = LocateATextField(myTextFieldLayout, txtPCName, 20, 130, 25);
-        txtPCID = LocateATextField(myTextFieldLayout, txtPCID, 20, 130, 50);
-        txtIP = LocateATextField(myTextFieldLayout, txtIP, 20, 130, 75);
-        txtFind = LocateATextField(myTextFieldLayout, txtFind, 20, 130, 120);
+        txtContactName  = LocateATextField(myTextFieldLayout, txtContactName, 20, 130, 25);
+        txtContactType = LocateATextField(myTextFieldLayout, txtContactType, 20, 130, 50);
+        txtPhoneNumber = LocateATextField(myTextFieldLayout, txtPhoneNumber, 20, 130, 75);
+        txtWebsiteEmail = LocateATextField(myTextFieldLayout, txtWebsiteEmail, 20, 130, 100);
+        txtFind = LocateATextField(myTextFieldLayout, txtFind, 10, 430, 25);
     }
 
     /**
     * Method with low coupling and high cohesion 
-    *    for adding individual textboxes:
+    *    for adding individual text boxes:
     *    - reduces overall code, especially in the
     *         LocateTextFields method.
     *    - makes this method re-usable with minimal
@@ -101,21 +106,44 @@ public class IPAddressDatabase extends Frame implements WindowListener
         return myTextField;
     }
 
+    
+    public void LocateTextAreas(SpringLayout myTextAreaLayout)
+    {
+        txtContactNote = LocateATextArea(myTextAreaLayout, txtContactNote, 5, 20, 130, 125);
+    }
+
+    /**
+    * Method with low coupling and high cohesion 
+    *    for adding individual text boxes:
+    *    - reduces overall code, especially in the
+    *         LocateTextFields method.
+    *    - makes this method re-usable with minimal
+    *         adjustment as it is moved from one
+    *         program to another.
+    */
+    public TextArea LocateATextArea(SpringLayout myTextAreaLayout, TextArea myTextArea, int height, int width, int x, int y)
+    {
+        myTextArea = new TextArea(height, width);
+        add(myTextArea);        
+        myTextAreaLayout.putConstraint(SpringLayout.WEST, myTextArea, x, SpringLayout.WEST, this);
+        myTextAreaLayout.putConstraint(SpringLayout.NORTH, myTextArea, y, SpringLayout.NORTH, this);
+        return myTextArea;
+    }
 
     //------------------------------------------------------------------------------------------
     // Method that manages the adding of multiple Buttons to the screen.
     // Each line requests the LocateAButton method to instantiate, add and place a specific Button  
     public void LocateButtons(SpringLayout myButtonLayout)
     {
-        btnNew = LocateAButton(myButtonLayout, btnNew, "New", 320, 25, 80, 25);
-        btnSave = LocateAButton(myButtonLayout, btnSave, "Save", 320, 50, 80, 25);
-        btnDel = LocateAButton(myButtonLayout, btnDel, "Delete", 320, 75, 80, 25);
-        btnFind = LocateAButton(myButtonLayout, btnFind, "Find", 320, 100, 80, 25);
+        btnNew = LocateAButton(myButtonLayout, btnNew, "New", 420, 90, 80, 25);
+        btnSave = LocateAButton(myButtonLayout, btnSave, "Save", 420, 115, 80, 25);
+        btnDel = LocateAButton(myButtonLayout, btnDel, "Delete", 420, 140, 80, 25);
+        btnFind = LocateAButton(myButtonLayout, btnFind, "Find", 420, 50, 80, 25);
         btnExit = LocateAButton(myButtonLayout, btnExit, "Exit", 320, 170, 80, 25);
-        btnFirst = LocateAButton(myButtonLayout, btnFirst, "|<", 140, 170, 30, 25);
-        btnPrev = LocateAButton(myButtonLayout, btnPrev, "<", 180, 170, 30, 25);
-        btnNext = LocateAButton(myButtonLayout, btnNext, ">", 220, 170, 30, 25);
-        btnLast = LocateAButton(myButtonLayout, btnLast, ">|", 260, 170, 30, 25);
+        btnFirst = LocateAButton(myButtonLayout, btnFirst, "|<", 400, 175, 30, 25);
+        btnPrev = LocateAButton(myButtonLayout, btnPrev, "<", 430, 175, 30, 25);
+        btnNext = LocateAButton(myButtonLayout, btnNext, ">", 460, 175, 30, 25);
+        btnLast = LocateAButton(myButtonLayout, btnLast, ">|", 490, 175, 30, 25);
     }
 
     /**
@@ -154,6 +182,7 @@ public class IPAddressDatabase extends Frame implements WindowListener
 
     public void windowClosed(WindowEvent we)
     {
+        
     }
 
     public void windowDeiconified(WindowEvent we)
